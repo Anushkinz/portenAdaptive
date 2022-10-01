e792e5c25163aaeabcf5881f3d394ae0f5f9709e
ANUAR <anuarprogramm@gmail.com>
2022-10-01 18:54:05 +0600

    init commit

---
+++ script.js
@@ -0,0 +1,13 @@
+let $slider = document.querySelector("#slider")
+let $prev = document.querySelector("#prev")
+let $next = document.querySelector("#next")
+let $content = document.querySelector("#content")
+let left = 0
+$next.addEventListener('click', function(){
+    left -= 310
+    $content.style.left = left + "px"
+})
+$prev.addEventListener('click', function(){
+    left += 310
+    $content.style.left = left + "px"
+})
